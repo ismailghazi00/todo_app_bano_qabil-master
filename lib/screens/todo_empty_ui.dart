112901efe4795c703aed0ea8edb76bd84a140b23
ismailghazi00 <ismailghazi00@gmail.com>
2023-06-11 21:53:47 +0500

    added shared_preferences

---
+++ lib/screens/todo_empty_ui.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/screens/todo_main.dart';
+
+class TodoEmptyScreenWidget extends StatelessWidget {
+  const TodoEmptyScreenWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      // appBar: _showAppBar(),
+      body: Center(
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(74, 74, 74, 10),
+              child: Image.asset(
+                "assets/checklist.png",
+                width: 227,
+                height: 227,
+              ),
+            ),
+            Text(
+              'What do you want to do today?',
+              style: textStyle(20),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Tap + to add your tasks',
+              style: textStyle(16),
+            ),
+            // const Expanded(child: SizedBox()),
+            // _showAddTaskButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
